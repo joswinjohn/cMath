fcccc5de89f49b0b4d2905f94d692cdfe84dde33
joswinjohn <65504682+joswinjohn@users.noreply.github.com>
2021-02-28 09:51:30 -0500

    Added Modulus, Square Root, Eponents, etc. (Also added System.Threading)

--- CalculatorDLL/Calculator/Calculator/Functions.cs
+++ CalculatorDLL/Calculator/Calculator/Functions.cs
@@ -27,5 +27,26 @@ namespace Calculator
         {
             return num1 / num2;
         }
+        public static double Modulo(double num1, double num2)
+        {
+            return num1 % num2;
+        }
+
+        //New arithmetic here
+        public static double Sqroot(double num1)
+        {
+            return Math.Sqrt(num1);
+        }
+
+        public static double Power(double num1, double num2)
+        {
+            return Math.Pow(num1, num2);
+        }
+
+
+        public static double Round(double num1)
+        {
+            return Math.Round(num1);
+        }
     }
 }
--- CalculatorNET/Calculator/Calculator/Program.cs
+++ CalculatorNET/Calculator/Calculator/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Threading;
 using Calculator;
 
 namespace Calculator
@@ -12,16 +13,33 @@ namespace Calculator
         static void Main(string[] args)
         {
         Start:
+            Console.Clear();
+
             string n1b, n2b;
             double n1 = 0.0;
             double n2 = 0.0;
 
+            Console.WriteLine("Type your First Number...");
+
             n1b = Console.ReadLine();
             bool isNumeric = double.TryParse(n1b, out _);
+            bool isEquation = n1b.Contains("+") || n1b.Contains("-") || n1b.Contains("*") || n1b.Contains("/");
 
-            if(isNumeric == true) {
+            if (isEquation == true) {
+                Console.WriteLine("Please enter a number(Not an equation)");
+                Thread.Sleep(2000);
+                goto Start;
+            } else if(isNumeric == true && isEquation == false) {
                 n1 = Convert.ToDouble(n1b);
+                Console.Clear();
+            }else {
+                Console.WriteLine("Enter a real number");
+                Thread.Sleep(1000);
+                Console.Clear();
+                goto Start;
             }
+
+            Console.WriteLine("Type your Second Number... (Type any random number to use sqrt function)");
 
             n2b = Console.ReadLine();
             bool isNumeric2 = double.TryParse(n2b, out _);
@@ -29,26 +47,98 @@ namespace Calculator
             if (isNumeric2 == true)
             {
                 n2 = Convert.ToDouble(n2b);
+                Console.Clear();
+            } else
+            {
+                Console.Clear();
+                goto Start;
             }
+
             Console.WriteLine("Choose an Operation:");
             Console.WriteLine("a = add");
             Console.WriteLine("s = subtract");
             Console.WriteLine("m = multiply");
             Console.WriteLine("d = divide");
+            Console.WriteLine("mod = modulus | remainder of num1 / num2");
+            Console.WriteLine("sqrt = sqare root | square root of num1");
+            Console.WriteLine("pow = Power | num1 ^ num2");
 
-            switch(Console.ReadLine())
+            switch (Console.ReadLine())
             {
                 case "a":
-                    Console.WriteLine($"{n1} + {n2} = " + (n1 + n2));
-                break;
+                    double a = Functions.Add(n1, n2);
+                    if (a == Convert.ToDouble("∞"))
+                    {
+                        Console.WriteLine("Number is too high");
+                    }
+                    else
+                    {
+                        Console.WriteLine(a);
+                    }
+                    break;
                 case "s":
-                    Console.WriteLine($"{n1} - {n2} = " + (n1 - n2));
+                    double s = Functions.Subtract(n1, n2);
+                    if (s == Convert.ToDouble("∞"))
+                    {
+                        Console.WriteLine("Number is too high");
+                    }
+                    else
+                    {
+                        Console.WriteLine(s);
+                    }
                     break;
                 case "m":
-                    Console.WriteLine($"{n1} * {n2} = " + (n1 * n2));
+                    double m = Functions.Multiply(n1, n2);
+                    if (m == Convert.ToDouble("∞"))
+                    {
+                        Console.WriteLine("Number is too high");
+                    }
+                    else
+                    {
+                        Console.WriteLine(m);
+                    }
                     break;
                 case "d":
-                    Console.WriteLine($"{n1} / {n2} = " + (n1 / n2));
+                    double d = Functions.Divide(n1, n2);
+                    if (d == Convert.ToDouble("∞"))
+                    {
+                        Console.WriteLine("Number is too high");
+                    }
+                    else
+                    {
+                        Console.WriteLine(d);
+                    }
+                    break;
+                case "mod":
+                    double mod = Functions.Modulo(n1, n2);
+                    if (mod == Convert.ToDouble("∞"))
+                    {
+                        Console.WriteLine("Number is too high");
+                    }
+                    else
+                    {
+                        Console.WriteLine(mod);
+                    }
+                    break;
+                case "sqrt":
+                    double sqrt = Functions.Sqroot(n1);
+                    if (sqrt == Convert.ToDouble("∞"))
+                    {
+                        Console.WriteLine("Number is too high");
+                    }
+                    else
+                    {
+                        Console.WriteLine(sqrt);
+                    }
+                    break;
+                case "pow":
+                    double pow = Functions.Power(n1, n2);
+                    if (pow == Convert.ToDouble("∞")) {
+                        Console.WriteLine("Number is too high");
+                    } else
+                    {
+                        Console.WriteLine(pow);
+                    }
                     break;
             }
 
